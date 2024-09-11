969010fbf3a670686f302caf4364b632b8ea74ea
chaitanya717 <chaudharichaitanya998@gmail.com>
2024-09-11 12:28:27 +0530

    dfgdfg

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,71 +1,115 @@
-variable "region" {
-    type = string
-    default = "sa-east-1"
-}
+variable "app_name" {}
+variable "enviroment" {}
 
-variable "main_cidr_block" {
-  type    = string
-  default = "10.0.0.0/16"
-}
+variable "cidr_block_vpc" {}
 
-variable "public_cidr_blocks" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
+variable "vpc_name" {}
+variable "public_subnet_cidr" {
+  type = list(string)
+  }
+variable "private_subnet_cidr" {
+  type = list(string)
+  }
+
+variable "internet_gateway_name" {}
+variable "route_table_name_private" {}
+variable "route_table_name_public" {}
+variable "nat_gateway_name" {}
+
+variable "iam-instance-profile-name" {}
+variable "iam-instance-role-name" {}
+variable "iam-instance-role-policy-name" {}
 
 
-variable "private_cidr_blocks" {
-  type    = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
-}
 
-variable "ecr_application_tier" {
-  type    = string
-  default = "app-application-tier"
-}
+variable "iam_instance_profile_name" {}
+variable "region" {}
 
-variable "ecr_presentation_tier" {
-  type    = string
-  default = "app-presentation-tier"
+variable "key_name" {}
+variable "public_key" {
+  type = string
+  sensitive = true
 }
+variable "launch_template_name_presentetion_tier" {}
+variable "instance_type_presentetion_tier" {}
+
+variable "launch_template_name_application_tier" {}
+variable "instance_type_application_tier" {}
+
+
+
+variable "alb_presentation_tier_name" {}
+variable "alb_presentation_tier_load_balancer_type" {}
+variable "alb_presentation_tier_enable_deletion_protection" {}
+
+variable "alb_application_tier_name" {}
+variable "alb_application_tier_load_balancer_type" {}
+variable "alb_application_tier_enable_deletion_protection" {}
+
 
 # rds variables
-variable "rds_db_admin" {
-  type = string
-  default = "admin"
-}
 
-variable "rds_db_password" {
-  type = string
-  default = "password"
-}
+# variable "rds_db_password" {
+#   type = string
+# }
 
 variable "multi_az" {
   type = bool
-  default = false
 }
 
-variable "db_name" {
-  type = string
-  default = "my_db"
-}
+# variable "db_name" {
+#   type = string
+# }
 
 variable "engine_version" {
   type = string
-  default = "5.7.31"
+  # default = "5.7.31"
 }
 
 variable "allocated_storage" {
   type = number
-  default = 10
+  # default = 10
 }
 
 variable "instance_class" {
   type = string
-  default = "db.t3.micro"
+  # default = "db.t3.micro"
 }
 
 variable "db_engine" {
-  default = "mysql"
+  type = string
+  # default = "mysql"
 }
 
+# auto scaling
+
+variable "asg-presentation-tier-name" {}
+variable "asg-presentation-tier-max_size" {
+    type = number
+}
+variable "asg-presentation-tier-min_size" {
+    type = number
+}
+variable "asg-presentation-tier-health_check_grace_period" {
+    type = number
+}
+variable "asg-presentation-tier-health_check_type" {}
+variable "asg-presentation-tier-desired_capacity" {
+   type = number
+}
+
+
+variable "asg-application-tier-name" {}
+variable "asg-application-tier-max_size" {
+   type = number
+}
+variable "asg-application-tier-min_size" {
+   type = number
+}
+variable "asg-application-tier-health_check_grace_period" {
+  type = number
+}
+variable "asg-application-tier-health_check_type" {}
+variable "asg-application-tier-desired_capacity" {
+   type = number
+}
